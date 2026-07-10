66b71b1474b237d1f61369533fff8ddf418a491c
Harshit <harshitkrsharma07@gmail.com>
2026-07-10 15:08:32 +0530

    Update db.js

--- BackEnd/Config/db.js
+++ BackEnd/Config/db.js
@@ -1,9 +1,8 @@
 const { Sequelize } = require('sequelize');
 
-const db = new Sequelize('Zomato', 'root', 'DGQmoGkQwmtRrwquDtKHWWJRzOYIFvjX', {
-
-    host: 'localhost',
+const db = new Sequelize('mysql://root:DGQmoGkQwmtRrwquDtKHWWJRzOYIFvjX@hayabusa.proxy.rlwy.net:20154/railway', {
     dialect: 'mysql',
+    logging: false
 });
 
 module.exports = db;
